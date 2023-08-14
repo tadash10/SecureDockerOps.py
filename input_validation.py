94c10e4bd73a56feb80db7e102b47e4248c8950c
T1 <126980610+tadash10@users.noreply.github.com>
2023-08-13 19:09:13 -0600

    Create input_validation.py

---
+++ input_validation.py
@@ -0,0 +1,11 @@
+def validate_image_name(image_name):
+    # Implement image name validation here (e.g., check if it's a valid Docker image name)
+    pass
+
+def validate_container_name(container_name):
+    # Implement container name validation here (e.g., check for name conflicts)
+    pass
+
+def validate_port_mappings(port_mappings):
+    # Implement port mapping validation here (e.g., check for conflicts, reserved ports, etc.)
+    pass
