e4892aa8185747d293df31f2a57ed2edf7116163
T1 <126980610+tadash10@users.noreply.github.com>
2023-08-13 19:10:33 -0600

    Create cleanup_utils.py

---
+++ cleanup_utils.py
@@ -0,0 +1,9 @@
+def cleanup_container(container):
+    # Implement container cleanup here
+    pass
+
+def cleanup_all_containers():
+    # Implement cleanup of all containers (e.g., after completion or when no longer needed)
+    pass
+
+# Implement graceful shutdown mechanism here
