61b6955553c305aaaebeea25be11056666189434
T1 <126980610+tadash10@users.noreply.github.com>
2023-08-13 19:09:41 -0600

    Create container_utils.py

---
+++ container_utils.py
@@ -0,0 +1,12 @@
+import docker
+from docker.errors import APIError, ImageNotFound
+
+def create_container(image_name, container_name, port_mappings=None):
+    # Implement container creation here
+    pass
+
+def start_container(container):
+    # Implement container start here
+    pass
+
+# Add other container management functions as needed
